65c61bf88e86e03378fc90c616d2b6c788ce0105
sangitakumari7 <sangitaroy4829@gmail.com>
2024-09-23 17:39:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const passwordBox = document.getElementById("password");
+const button = document.getElementById("btn");
+const copyButton = document.getElementById("copy");
+
+const generatePassword = ()=>{
+
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$^&*()_+}{|\/><=-`~[]";
+
+    let password = "";
+    const lenthofPassword = 8;
+
+    for(let i = 0; i < lenthofPassword; i++){
+        password +=  chars.charAt (Math.floor(Math.random() *  chars.length))
+    }
+
+    passwordBox.value = password;
+};
+
+button.addEventListener("click",generatePassword);
+
+copyButton.addEventListener("click",()=>{
+    const passwordCopy = passwordBox.value.trim();
+    if(!passwordCopy){
+        alert("No password to copy! Please generate a password first.");
+        return;
+    }
+
+
+    navigator.clipboard.writeText(passwordCopy).then(()=>{
+        alert("Password copied to clipboard!");
+    });
+});
